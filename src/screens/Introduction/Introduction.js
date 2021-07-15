4be9a6b68bcf82938d552fe273ed0ae8c2077c84
Pham Duy Hoan <phamduyhoan95@gmail.com>
2021-07-15 22:40:44 +0700

    Add Introduction without css

--- src/screens/Introduction/Introduction.js
+++ src/screens/Introduction/Introduction.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import NavBar from './components/NavBar'
+import WideSectionHeader from './components/WideSectionHeader'
+import PortfolioList from './components/PortfolioList'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
 const Introduction = () => {
   return (
-    <div> This is Introduction Page </div>
+    <div>
+      <div>
+        <NavBar />
+        <WideSectionHeader />
+        <PortfolioList />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
---
+++ src/screens/Introduction/components/PortfolioListItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import PortfolioItemDetailModal from './PortfolioItemDetailModal.jsx'
+
+class PortfolioListItem extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className="col-sm-4 portfolio-item">
+            <a href={'#' + this.props.modalName} className="portfolio-link" data-toggle="modal">
+                <div className="caption">
+                    <div className="caption-content">
+                        <i className="fa fa-search-plus fa-3x"></i>
+                    </div>
+                </div>
+                <img src={this.props.imgUrl} className="img-responsive" alt="" />
+            </a>
+        </div>
+        <PortfolioItemDetailModal imgUrl={this.props.imgUrl} modalName={this.props.modalName}/>
+      </div>
+    )
+  }
+}
+PortfolioListItem.propTypes = {
+  modalName: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired
+}
+export default PortfolioListItem

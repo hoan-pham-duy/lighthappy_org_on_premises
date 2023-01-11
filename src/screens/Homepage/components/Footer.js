0e1c84dc618293c87c9f7c46b7cdab2201d192fc
Pham Duy Hoan <phamduyhoan95@gmail.com>
2023-01-11 12:01:54 +0700

    Update src/screens/Homepage/components/Footer.js with personal links

--- src/screens/Homepage/components/Footer.js
+++ src/screens/Homepage/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 class Footer extends React.Component {
   render () {
@@ -14,7 +14,7 @@ class Footer extends React.Component {
               <div className='col-lg-4 mb-5 mb-lg-0'>
                 <h4 className='text-uppercase mb-4'>Location</h4>
                 <p className='lead mb-0'>
-                  Ha Dong District, Ha Noi
+                  Hoang Mai District, Ha Noi
                   <br />
                   Viet Nam
                 </p>
@@ -22,10 +22,10 @@ class Footer extends React.Component {
               {/* Footer Social Icons */}
               <div className='col-lg-4 mb-5 mb-lg-0'>
                 <h4 className='text-uppercase mb-4'>Around the Web</h4>
-                <a className='btn btn-outline-light btn-social mx-1' href='https://www.facebook.com/hoan.phamduy.1 target="_blank"'><FontAwesomeIcon icon={faFacebook} /></a>
-                <a className='btn btn-outline-light btn-social mx-1' href='#!'><i className='fab fa-fw fa-twitter' /></a>
-                <a className='btn btn-outline-light btn-social mx-1' href='#!'><i className='fab fa-fw fa-linkedin-in' /></a>
-                <a className='btn btn-outline-light btn-social mx-1' href='#!'><i className='fab fa-fw fa-dribbble' /></a>
+                <a className='btn btn-outline-light btn-social mx-1' href='https://www.facebook.com/hoan.phamduy.1' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faFacebook} /></a>
+                <a className='btn btn-outline-light btn-social mx-1' href='https://github.com/hoan-pham-duy' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faGithub} /></a>
+                <a className='btn btn-outline-light btn-social mx-1' href='https://www.linkedin.com/in/pham-duy-hoan-10a68b13a/' target='_blank' rel='noreferrer'><FontAwesomeIcon icon={faLinkedin} /></a>
+                {/* <a className='btn btn-outline-light btn-social mx-1' href='#!'><i className='fab fa-fw fa-dribbble' /></a> */}
               </div>
             </div>
           </div>

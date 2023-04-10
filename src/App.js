4427c75428942cb7291e615e6ec5f8bd49bd57c5
Pham Duy Hoan <phamduyhoan95@gmail.com>
2023-04-10 09:43:51 +0000

    Add /about-me path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 
 import Homepage from './screens/Homepage/Homepage.js'
+import AboutMe from './screens/AboutMe/AboutMe'
 import RealTimeChatRoom from './screens/RealTimeChatRoom/RealTimeChatRoom.js'
 import PdfTools from './screens/PdfTools/PdfTools.js'
 import SignIn from './screens/SignIn/SignIn.js'
@@ -14,6 +15,7 @@ function App () {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={() => <Homepage />} />
+        <Route exact path="/about-me" render={() => <AboutMe />} />
         <Route exact path="/real-time-chat-room" render={() => <RealTimeChatRoom />} />
         <Route exact path="/pdf-tools" render={() => <PdfTools />} />
         <Route exact path="/signin" render={() => <SignIn />} />
---
+++ src/screens/AboutMe/AboutMe.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { Document, Page } from 'react-pdf'
+import ResumePdf from './Resume_DevOps_AWS_Resume_06_APR_2023.pdf'
+
+function AboutMe () {
+  const [numPages, setNumPages] = useState(null)
+  // const [pageNumber, setPageNumber] = useState(1)
+  // const pageNumber = 1
+
+  function onDocumentLoadSuccess ({ numPages }) {
+    // setPageNumber(1)
+    setNumPages(numPages)
+  }
+
+  return (
+    <div>
+      <Document file={ResumePdf} onLoadSuccess={onDocumentLoadSuccess} options={{ workerSrc: 'pdf.worker.js' }}>
+      {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+      ))}
+      </Document>
+    </div>
+  )
+}
+
+export default AboutMe

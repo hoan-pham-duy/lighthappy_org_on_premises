78d4d378a7145c613be108acac82315d8f23ccc2
Pham Duy Hoan <phamduyhoan95@gmail.com>
2023-04-10 09:59:06 +0000

    Fix width display /about-me

--- src/screens/AboutMe/AboutMe.js
+++ src/screens/AboutMe/AboutMe.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Document, Page } from 'react-pdf'
 import ResumePdf from './Resume_DevOps_AWS_Resume_06_APR_2023.pdf'
 
+import './AboutMe.css'
+
 function AboutMe () {
   const [numPages, setNumPages] = useState(null)
   // const [pageNumber, setPageNumber] = useState(1)
@@ -13,7 +15,7 @@ function AboutMe () {
   }
 
   return (
-    <div>
+    <div className="Example__container__document">
       <Document file={ResumePdf} onLoadSuccess={onDocumentLoadSuccess} options={{ workerSrc: 'pdf.worker.js' }}>
       {Array.from(new Array(numPages), (el, index) => (
               <Page key={`page_${index + 1}`} pageNumber={index + 1} />
